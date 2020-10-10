929bacf90a99ed1462128bb6265bd3be68ce86c6
alqaz <alqaz@outlook.com>
2020-10-10 23:41:16 +0800

    add demo codes

--- demo/main.c
+++ demo/main.c
@@ -5,7 +5,7 @@
 int main(int argc, const char** argv)
 {
     const char* sg_lib_v = sg_lib_version();
-    get_odd_drivers();
+    Odd_DriverInfo* odds =  get_odd_drivers();
     printf("sg_version:[%s]\n", sg_lib_v);
     return 0;
 }
